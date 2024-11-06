3176dcef2f8dc4b12b97e6c944927ef58f5a913d
Luiz Carlos <argentinaluiz@gmail.com>
2024-11-06 15:52:44 -0300

    cap03-aula-06-definicao-de-variaveis-de-ambiente

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.get('/test-db', async function(req, res){
     host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
-    database : process.env.DB_DATABASE
+    database : process.env.DB_DATABASE,
   });
   connection.connect(error => {
     if (error) {
